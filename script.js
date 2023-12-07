b83457f2449c3acb93c35fbf9891f42af8a6ef41
AMIT YADAV <89079154+Amit09Yadav@users.noreply.github.com>
2023-12-07 12:46:24 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function signup(event) {
     event.preventDefault();
 
-    // Get form values
     var firstName = document.getElementById('firstName').value;
     var lastName = document.getElementById('lastName').value;
     var username = document.getElementById('username').value;
@@ -14,13 +13,10 @@ function signup(event) {
     var pincode = document.getElementById('pincode').value;
     var userType = document.getElementById('userType').value;
 
-    // Check if password and confirm password match
     if (password !== confirmPassword) {
         alert('Passwords do not match');
         return;
     }
-
-    // Save user details to localStorage (for simplicity, in a real app, you'd use a server)
     var userDetails = {
         firstName: firstName,
         lastName: lastName,
@@ -34,23 +30,17 @@ function signup(event) {
     };
 
     localStorage.setItem('userDetails', JSON.stringify(userDetails));
-
-    // Redirect to the login page (for simplicity, in a real app, you'd redirect to a login page)
     window.location.href = 'login.html';
 }
 
 function login(event) {
     event.preventDefault();
-
-    // Get form values
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    // For simplicity, check if the username and password match with the stored user details
     var storedUserDetails = JSON.parse(localStorage.getItem('userDetails'));
 
     if (storedUserDetails && username === storedUserDetails.username && password === 'password') {
-        // Redirect to the dashboard with the username and user type
         window.location.href = 'dashboard.html?username=' + username + '&userType=' + storedUserDetails.userType;
     } else {
         alert('Invalid credentials');
@@ -62,8 +52,6 @@ function displayDashboard() {
     var username = urlParams.get('username');
     var userType = urlParams.get('userType');
     document.getElementById('username').textContent = username;
-
-    // Display user details from localStorage
     var storedUserDetails = JSON.parse(localStorage.getItem('userDetails'));
     var userDetailsList = document.getElementById('userDetails');
 
@@ -74,7 +62,6 @@ function displayDashboard() {
     }
 }
 
-// Check if on the dashboard page and display details
 if (window.location.pathname.includes('dashboard.html')) {
     displayDashboard();
 }
